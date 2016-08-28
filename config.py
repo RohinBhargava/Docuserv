272b36fa89f677bfbbf78e2b1ad7fa8b6b32c10b
Rohin Bhargava <rohin@uchicago.edu>
2016-08-28 18:42:05 -0500

    DB location

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'tau-kappa-phi'
 DEBUG = True
-SQLALCHEMY_DATABASE_URI  = 'sqlite:////home/rohin/Docuserv/zd.db'
+SQLALCHEMY_DATABASE_URI  = 'sqlite:////home/legmonkey/Docuserv/zd.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECURITY_PASSWORD_HASH = 'pbkdf2_sha512'
 SECURITY_PASSWORD_SALT = 'zpoa'
