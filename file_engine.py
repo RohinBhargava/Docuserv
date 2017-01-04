1cc851f7344afa466bfc97c6e85e3bfc5affb1b2
Rohin Bhargava <rohin@uchicago.edu>
2017-01-04 05:18:12 -0600

    help me please

--- file_engine.py
+++ file_engine.py
@@ -140,7 +140,7 @@ def process_file(conversion_image, istext, isxml, path):
         os.system('enscript --word-wrap --no-header ' + path + conversion_image + ' -o ' + path + ps_image + ' >> ' + path + 'logs/' + conversion_image + '.log 2>&1')
         os.system('ps2pdf ' + path + ps_image + ' ' + path + pdf_image + ' >> ' + path + 'logs/' + conversion_image + '.log 2>&1')
     if isxml:
-        os.system('sudo chown -R --reference="$HOME" ~/.config >> ' + path + 'logs/' + conversion_image + '.log 2>&1')
+        os.system('chown -R --reference="$HOME" ~/.config >> ' + path + 'logs/' + conversion_image + '.log 2>&1')
         os.system('soffice --headless --convert-to pdf ' + path + conversion_image + ' --outdir ' + path + ' >> ' + path + 'logs/' + conversion_image + '.log 2>&1')
     a = 0
     i = 0
