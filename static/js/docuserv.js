69ca611458846899b5f72fcc16ea2cb4d1daedb6
Rohin Bhargava <rohin@uchicago.edu>
2017-10-19 19:35:20 -0500

    fadsfdas

--- static/js/docuserv.js
+++ static/js/docuserv.js
@@ -348,7 +348,7 @@ function docView(name, hashpath, size) {
         data : {'path': hashpath},
         success: function(data) {
           console.log("gello")
-          // $("#modalDocBod").html('<object data=data width="900" height="600" type="application/pdf"> </object>');
+          $("#modalDocBod").html('<object data=data width="900" height="600" type="application/pdf"> </object>');
         }
       });
     // $.getJSON(, {
