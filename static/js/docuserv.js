2e09d4b3d951ad612224efbd85a9258579231547
Rohin Bhargava <legmonkey@Rohins-MacBook-Air.local>
2018-02-10 22:38:56 -0500

    poopy

--- static/js/docuserv.js
+++ static/js/docuserv.js
@@ -355,6 +355,7 @@ function docView(name, hashpath, size) {
         // contentType: 'application/pdf',
         data : {'path': hashpath},
         success: function(data) {
+          console.log('I fucked around and found a plug');
           $("#modalDocBod").html('<object data=data width="900" height="600" type="application/pdf" data="' + data + '"></object>');
         }
       });
